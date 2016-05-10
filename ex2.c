4f99802e079c1310c20219fe7f1c83cb46be419d
bruchong <bruchong@esXXXX.esisar.grenoble-inp.fr>
2016-05-10 11:32:22 +0200

    sdfuiqui

--- ex2.c
+++ ex2.c
@@ -1,49 +1,57 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 int compteur=0;
 
 void increment (int id) {
-	compteur++;
+  compteur++;
+  printf("%d\n", compteur);
 }
 
 int main (void)
 {
-	pid_t pid;
+  pid_t pid;
 
-	pid = fork();
-	if (pid == -1) {
-		perror ("Erreur de fork");
-		exit(2);
-	}
-	else if (pid == 0) {
-	  	pid = fork();
-		if (pid == -1) {
-		  perror ("Erreur de fork");
-		  exit(2);
-		}
+  pid = fork();
+  if (pid == -1) {
+    perror ("Erreur de fork");
+    exit(2);
+  }
+  else if (pid == 0) {
+    pid = fork();
+    if (pid == -1) {
+      perror ("Erreur de fork");
+      exit(2);
+    }
 
-		else if (pid == 0) {
-		  signal(SIGALRM, fonction(increment));
-		  while(1) {
-		    if (compteur == 60){
-		      compteur = 0;
-		      kill(getppid(), SIGALRM);
-		    }  
-		  }
+    else if (pid == 0) { //Comptage des secondes
+      signal(SIGALRM, increment);
+      while(1) {
+	alarm(1);
+	sleep(1);
+	if (compteur == 60){
+	  compteur = 0;
+	  kill(getppid(), SIGALRM);
+	}  
+      }
+    }
 
-		  else {
-		    signal(SIGALRM, fonction(increment));
-		    while(1) {		 
-		      if (compteur == 60){
-			compteur = 0;
-			kill(getppid(), SIGALRM);
-		      }
-		    }
-		  }
+    else { //Comptage des minutes 
+      signal(SIGALRM, increment);
+      while(1) {		 
+	if (compteur == 60){
+	  compteur = 0;
+	  kill(getppid(), SIGALRM);
 	}
-	else {
-	  signal(SIGALRM, fonction(increment));
-	  while(1);
-	}
+      }
+    }
+  }
+  else { //Comptage des heures
+    signal(SIGALRM, increment);
+    while(1);
+  }
 }
