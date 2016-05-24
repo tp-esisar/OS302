989033a950c335b9a6abbc177248a87e2130518f
bruchong <bruchong@esXXXX.esisar.grenoble-inp.fr>
2016-05-24 16:08:20 +0200

    youhou

--- TP7/exo2.c
+++ TP7/exo2.c
@@ -3,61 +3,62 @@
  * Utilisation des segments de memoire partagee                      *
  *********************************************************************/
 /*
-Exercice 2
-Usage: ./exo2.e
+Usage (sans argument) : ./exo2.e
 
-Créé un segment partagé et des processus père et fils.
+Créé un segment partagé entre des processus père et fils.
 Le père écrit un message dans le segment partagé, et le fils l'affiche à l'écran.
 
+Etudiants : Bertrand - Bruchon
 */
+
 #include <string.h>
 #include <unistd.h>
-#include "segment_memoire.h"
 #include <sys/wait.h>
+#include "segment_memoire.h"
 
 int main() {
   
-  int pid;      // le PID du processus
-  char *mem;    // pointeur vers le segment memoire
-  int shmid;    // l'identificateur du segment memoire
-  char* nom = "exo2.c";
-  char* message = "youhouuu~!";
+  int pid;      					// le PID du processus
+  char *mem;    					// pointeur vers le segment memoire
+  int shmid;    					// l'identificateur du segment memoire
+  char* nom = "exo2.c";				// Nom de fichier pour générer la clé
+  char* message = "youhouuu~!";		// Message à partager
+
   //Creation du segment de memoire partagee
-  shmid = cree_segment(100,nom,42);
+  shmid = cree_segment(100,nom,42); //Création d'un segment partagé d'une taille de 100 octets
   if(shmid == -1) {
     fprintf(stderr,"erreur création segment");
     exit(EXIT_FAILURE);
   }
   
-  // creation du processus fils
+  //Creation du processus fils
   pid = fork();
   if (pid == -1) {
     perror("impossible de creer un processu fils!");
     exit(-1);
   }
-  else if (pid == 0) {
+
+  else if (pid == 0) { // Je suis le fils !
     sleep(2);
-    // je suis le fils!
-    // a completer : s'attacher au segment et affichage de son contenu
-    mem = shmat(shmid,NULL,0);
-    printf("Fils: affichage du segment partagé: \"%s\"\n",mem);
-afficher_info_segment(shmid);
-    
+    mem = shmat(shmid,NULL,SHM_RDONLY); //S'attacher au segment partagé en lecture seulement
+    printf("Fils: affichage du segment partagé: \"%s\"\n\nInfo du SMP fils :\n",mem); //Affichage de son contenu	
+	afficher_info_segment(shmid); //Affichage des informations du segment partagé
+	shmdt(mem);
   }
-  else {
-    // je suis le pere!
-    // attachement et ecriture sur le segment de memoire partagee
-    printf("Pere: ecriture dans le segment partagé:\n");
+
+  else { // Je suis le pere !
+    printf("Pere: ecriture dans le segment partagé : %s\n", message);
+	//Attachement et ecriture sur le segment de memoire partagee
     mem = shmat(shmid,NULL,0);
     memcpy(mem,message,strlen(message)*sizeof(char));
-    //attendre la fin du fils + detacher le segment et le detruire
+    //Attendre la fin du fils + detacher le segment et le detruire
     wait(NULL);
-	
+	printf("\nMon fils est mort !\n\nInfos du SMP pere\n");
+	afficher_info_segment(shmid); //Affichage des informations du segment partagé
+	shmdt(mem);
     detruire_segment(shmid);
-    
-    
-    // ...
   }  
 
   return 0 ;
 }
+
---
+++ TP7/readme.txt
@@ -0,0 +1,11 @@
+---------- TP7 OS302 ----------
+
+----- Auteurs -----
+Alexis BERTRAND
+Guillaume BRUCHON
+
+----- Compilation -----
+Ex1 : make exo1.e
+Ex2 : make exo2.e
+
+-------------------------------
