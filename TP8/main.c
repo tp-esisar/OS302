62d54e1c00058c0716051920e770ca8a227858a4
zipper97412 <zipper97412@gmail.com>
2016-05-25 15:42:46 +0200

    youhouuuuuuu

--- TP8/main.c
+++ TP8/main.c
@@ -4,36 +4,54 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-
 #define NB_FILS 6
+
+struct 
 
 int main (int argc, char* argv[])
 {
-	pid_t fils[NB_FILS+1];
+	pid_t fils[NB_FILS];
 	pid_t pere = getpid();
-
+	char* nom = "main.c";
+	char *mem;    					// pointeur vers le segment memoire
+	key_t cle;
+	if((cle = ftok(nom,42)) == -1) {
+		printf("Erreur de ftok\n");
+		exit(EXIT_FAILURE);
+	}
+  	int semid;
+  	if((semid = semget(cle,1,IPC_CREAT|0666)) == -1) {
+  		printf("Erreur de semget\n");
+  		exit(EXIT_FAILURE);
+  	}
+  	if(semctl(semid,0,SETVAL,2) == -1) {
+  		printf("Erreur SETVAL à 2\n");
+  		exit(EXIT_FAILURE);
+  	}
 	//Création des fils
 	//Remarque : Comme le fils sort du FOR directement après sa création, l'indice i correspond à son numéro de fils.
 	printf("Naissance des fils\n");
-	for (i=1; i<=NB_FILS; i++) {
+	for (i=0; i<NB_FILS; i++) {
 		fils[i] = fork();
 		if (fils[i] == 0)
 			break;
 	}
-
+	
 	// --- Partie du programme pour le PERE ---
 	if (pere == getpid()) { 
-
-
+		mem = shmat(shmid,NULL,SHM_RDONLY);
+		int i = 0;
+		while(
+		
 	}
 
 	// --- Partie du programme pour les FILS ---
 	else {
-
-
-
-
-
+		
+		
+		
+		
+		
 	}
 	
 	return 0;
