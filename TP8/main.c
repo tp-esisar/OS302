727ddfc13b3211ad92ce330b210ef51637ba9ac4
bruchong <bruchong@esXXXX.esisar.grenoble-inp.fr>
2016-05-25 15:38:49 +0200

    huio

--- TP8/main.c
+++ TP8/main.c
@@ -4,13 +4,27 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include "segment_memoire.h"
 
 #define NB_FILS 6
+
+int descendre = 0;
+
+void descente (int i) {
+	descendre = 0;
+}
 
 int main (int argc, char* argv[])
 {
 	pid_t fils[NB_FILS+1];
 	pid_t pere = getpid();
+
+	//Creation du segment de memoire partagee
+	shmid = cree_segment(2*sizeof(pid_t),nom,42);
+	if(shmid == -1) {
+		fprintf(stderr,"erreur création segment");
+		exit(EXIT_FAILURE);
+	}
 
 	//Création des fils
 	//Remarque : Comme le fils sort du FOR directement après sa création, l'indice i correspond à son numéro de fils.
@@ -29,10 +43,49 @@ int main (int argc, char* argv[])
 
 	// --- Partie du programme pour les FILS ---
 	else {
+		struct sembuf operation;
+		int pid = getpid();
+		sleep(1);
+
+		signal(SIGUSR1,descente);
+		
+		operation.sem_num = 0;
+		operation.sem_op = -1;
+		operation.sem_flg = 0;
+		if (semop(semid, &operation, 1) == -1) {
+			fprintf(stderr,"erreur de semaphore");
+			exit(EXIT_FAILURE);
+		}
+		printf("Fils %d : Je monte avec le numero %d", i, operation.sem_num);
+
+		if ((mem = shmat(shmid,NULL,0)) == (char*)-1) {
+			fprintf(stderr,"erreur ouverture segment");
+			exit(EXIT_FAILURE);
+		}
+		memcpy(mem + (operation.sem_num+1)*sizeof(pid), &pid, sizeof(pid));
+		printf("Fils %d : J'ai ecris mon pid %d", i, pid);
+		shmdt(mem);
+		
+		while(descendre == 0);
+
+		operation.sem_num = 0;
+		operation.sem_op = 1;
+		operation.sem_flg = 0;
+		if (semop(semid, &operation, 1) == -1) {
+			fprintf(stderr,"erreur de semaphore");
+			exit(EXIT_FAILURE);
+		}
+		printf("Fils %d : Je descend", i);
+		
+		
+		
+		
+		
+		
 
 
 
-
+		
 
 	}
 	
